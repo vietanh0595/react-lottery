700ee7c85e56ccc7197a0eac55387a087006400c
unknown <duongvietanh0495@gmail.com>
2022-12-10 13:14:04 +0700

    handle entering lottery players

--- src/lottery.js
+++ src/lottery.js
@@ -2,42 +2,70 @@ import web3 from "./web3";
 
 const address = '0x7241102b30564CeE2927AbD7F75700DAC77a69bb';
 const abi = [
-    { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
-    {
-      inputs: [],
-      name: 'enter',
-      outputs: [],
-      stateMutability: 'payable',
-      type: 'function'
-    },
-    {
-      inputs: [],
-      name: 'getPlayers',
-      outputs: [Array],
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      inputs: [],
-      name: 'manager',
-      outputs: [Array],
-      stateMutability: 'view',
-      type: 'function'
-    },
-    {
-      inputs: [],
-      name: 'pickWinner',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function'
-    },
-    {
-      inputs: [Array],
-      name: 'players',
-      outputs: [Array],
-      stateMutability: 'view',
-      type: 'function'
-    }
-  ];
+  {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+  },
+  {
+      "inputs": [],
+      "name": "enter",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+  },
+  {
+      "inputs": [],
+      "name": "getPlayers",
+      "outputs": [
+          {
+              "internalType": "address payable[]",
+              "name": "",
+              "type": "address[]"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [],
+      "name": "manager",
+      "outputs": [
+          {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [],
+      "name": "pickWinner",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "name": "players",
+      "outputs": [
+          {
+              "internalType": "address payable",
+              "name": "",
+              "type": "address"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  }
+];
 
   export default new web3.eth.Contract(abi, address);
